7115ccffda49832b3d5689cac4b006568bc1a71d
David Valdez <me@gnuget.org>
2011-03-06 21:17:12 -0600

    little fixes in the validation for custom types

--- js/mf_admin.js
+++ js/mf_admin.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
   //Custom Validataion methods 
   jQuery.validator.addMethod( "lowercase", function(value, element) {
-    return this.optional(element) || /[0-9a-z\_]+/.test(value);
+    return this.optional(element) || /^[0-9a-z\_]+$/.test(value);
   },'Only  are accepted lowercase characters,numbers or underscores');
 
   $('#addPostType').validate({meta:"validate"});
