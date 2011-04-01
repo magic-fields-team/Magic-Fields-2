8e31e022a7a092a9885b4f004e7d817727cb18e9
David Valdez <me@gnuget.org>
2011-04-01 00:54:21 -0600

    adding the skeleton of all of the field types

---
+++ field_types/image_field/image_field.php
@@ -0,0 +1,40 @@
+<?php
+// initialisation
+global $mf_domain;
+
+
+// class with static properties encapsulating functions for the field type
+
+class image_field extends mf_field {
+
+  public $allow_multiple = TRUE;
+  public $has_properties = TRUE;
+  
+  public function _update_description(){
+    global $mf_domain;
+    $this->description = __("Simple image input",$mf_domain);
+  }
+  
+  public function _options1(){
+    global $mf_domain;
+    
+    $data = array(
+      'option'  => array(
+        'type'  => array(
+          'type'        =>  'text',
+          'id'          =>  'uno',
+          'label'       =>  'opcion 1',
+          'name'        =>  'uno',
+          'default'     =>  '',
+          'description' =>  __( 'aqui una descripcion', $mf_domain ),
+          'value'       =>  '',
+          'div_class'    => 'clase1',
+          'class'       => 'vemos1'
+        )
+      )
+    );
+    
+    return $data;
+  }
+  
+}
---
+++ field_types/multiline_field/multiline_field.php
@@ -0,0 +1,33 @@
+<?php
+
+global $mf_domain;
+
+// initialisation
+
+class multiline_field extends mf_field {
+
+  public static $allow_multiple = TRUE;
+  public static $has_properties = TRUE;
+  public static $description = __("A TinyMCE editor with the same properites as the Wordpress visual editor, for editing rich HTML-based content");
+  
+  public static function post_ui($field, $field_value, $form_field) {
+
+  }
+  
+  public static function create_ui($field) {
+    
+  }
+  
+  public static function display() {
+    
+  }
+  
+  public static function get($field, $value) {
+    
+  }
+  
+  public static function set() {
+    
+  }
+}
+?>
