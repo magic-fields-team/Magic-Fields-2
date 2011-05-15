b497a9f3f84007db5709032224dc928a74b30993
hunk <ing.edgar@gmail.com>
2011-05-14 19:34:02 -0500

    reorder js file

--- main.php
+++ main.php
@@ -149,9 +149,9 @@ if( is_admin() ) {
       //Adding Css files for the post-new.php section (where is created a new post in wp)
       if( strstr( $_SERVER['REQUEST_URI'], 'post-new.php' ) !== FALSE  || strstr( $_SERVER['REQUEST_URI'],  'wp-admin/post.php') !== FALSE ) {
         wp_enqueue_style( 'mf_field_base', MF_BASENAME.'css/mf_field_base.css' );
-        wp_enqueue_script( 'tmpl', MF_BASENAME.'js/third_party/jquery.tmpl.js');
-        wp_enqueue_script( 'mf_sortable_groups', MF_BASENAME.'js/mf_sortable_groups.js', array( 'jquery-ui-sortable' ) );
+        wp_enqueue_script( 'tmpl', MF_BASENAME.'js/third_party/jquery.tmpl.js');       
         wp_enqueue_script( 'mf_field_base', MF_BASENAME.'js/mf_field_base.js'); 
+ wp_enqueue_script( 'mf_sortable_groups', MF_BASENAME.'js/mf_sortable_groups.js', array( 'jquery-ui-sortable' ) );
 
       
         //Loading any custom field  if is required 
