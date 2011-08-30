a41a2eacc9efa6ca88c1ac931bd1c733e4fa0d92
David Valdez <me@gnuget.org>
2011-08-30 00:30:13 -0500

    adding a "None" value into the related type field fixes #11

--- field_types/related_type_field/related_type_field.php
+++ field_types/related_type_field/related_type_field.php
@@ -9,7 +9,7 @@ class related_type_field extends mf_custom_fields {
 
   public $allow_multiple = TRUE;
   public $has_properties = TRUE;
-  
+
   public function _update_description(){
     global $mf_domain;
     $this->description = __("This field allows to do relations with other post type",$mf_domain);
@@ -64,6 +64,20 @@ class related_type_field extends mf_custom_fields {
           'value'       =>  '',
           'div_class'   => '',
           'class'       => ''
+        ),
+
+        'notype'  => array(
+          'type'        =>  'text',
+          'id'          =>  'notype',
+          'label'       =>  __('Default option (when no related type has been set)',$mf_domain),
+          'name'        =>  'mf_field[option][notype]',
+          'default'     =>  '',
+          'options'     => '',
+          'add_empty'   => false,
+          'description' =>  '',
+          'value'       =>  '',
+          'div_class'   => '',
+          'class'       => ''
         )
       )
     );
@@ -72,6 +86,14 @@ class related_type_field extends mf_custom_fields {
   }
 
   public function display_field( $field, $group_index = 1, $field_index = 1 ) {
+    global $mf_domain;
+
+    //If is not required this field be added a None value
+    $notype = "";
+    if( !$field['requiered_field'] ) {
+      $notype = ( !empty($field['options']['notype']) ) ? $field['options']['notype'] : __( "-- None --" , $mf_domain );
+    }
+
     $output = '';
 
     $type        = $field['options']['post_type'];
@@ -84,6 +106,11 @@ class related_type_field extends mf_custom_fields {
     $value = $field['input_value'];
 
     $output .= sprintf('<select class="dropdown_mf" id="%s" name="%s" >',$field['input_id'],$field['input_name']);
+
+    if( $notype != "" ) {
+      $output .= "<option value=''>$notype</option>";
+    }
+  
     foreach($options as $option) {
       $check = ($option->ID == $value) ? 'selected="selected"' : '';
 
@@ -98,5 +125,4 @@ class related_type_field extends mf_custom_fields {
 
     return $output;
   }
-  
 }
