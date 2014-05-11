43856eb967cf06d04684b8e8d1406173b380bf1c
Edgar G (@hunk) <ing.edgar@gmail.com>
2014-05-11 12:09:40 -0500

    fix problem with quantity in register post type

--- mf_register.php
+++ mf_register.php
@@ -71,7 +71,7 @@ class mf_register{
       register_post_type($name,$option);
 
       //add unique post type
-      if ($p['core']['quantity']) {
+      if (isset($p['core']['quantity']) && $p['core']['quantity'] == 1) {
         array_push($mf_pt_unique, "edit.php?post_type=".$name);
       }
      
